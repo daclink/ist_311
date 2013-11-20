2307cedf3dfad75b2b39c85bfcc8eafbf652170a
daclink <dclinkenbeard@omniupdate.com>
2013-11-20 10:27:32 -0800

    testing routs and such

---
+++ routes/routes.js
@@ -0,0 +1,21 @@
+/* 
+ * Author : Drew A. Clinkenbeard
+ * YourTwoPercent.org
+ * Routes for pages.
+ */
+var AppRouter = Backbone.Router.extend({
+    routes: {
+        "/about": "about",
+        "*actions": "defaultRoute" // matches http://example.com/#anything-here
+    },
+    about: function( ) {
+        // Note the variable in the route definition being passed in here
+        alert( "ABOUT!");
+        pageLoader("about");
+    },
+    defaultRoute: function( ){
+        pageLoader("home");
+        
+    }
+});
+           
