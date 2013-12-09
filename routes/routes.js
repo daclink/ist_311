94d88bd6f41b20f1f1c615a260cdfa134ea088fd
daclink <dclinkenbeard@omniupdate.com>
2013-12-09 14:24:48 -0800

    added routes

--- routes/routes.js
+++ routes/routes.js
@@ -7,21 +7,54 @@ var AppRouter = Backbone.Router.extend({
     routes: {
         "about": "about",
         "start_two": "start_two",
+        "once_start_two": "once_start_two",
+        "span_start_two": "span_start_two",
+        "always_start_two": "always_start_two",
         "restart_two": "restart_two",
-        "increase_two" : "increase_two",
+        "once_restart_two": "once_restart_two",
+        "span_restart_two": "span_restart_two",
+        "always_restart_two": "always_restart_two",
+        "increase_two": "increase_two",
         "*actions": "defaultRoute" // matches http://example.com/#anything-here
     },
     start_two: function( ) {
         // Note the variable in the route definition being passed in here
+        console.log("in routes, firing start_two");
         pageLoader("start_two");
+    },
+    once_start_two: function( ) {
+        // Note the variable in the route definition being passed in here
+        console.log("in routes, firing Once_start_two");
+        pageLoader("once_start_two");
+    },
+    span_start_two: function( ) {
+        // Note the variable in the route definition being passed in here
+        pageLoader("span_start_two");
+    },
+    always_start_two: function( ) {
+        // Note the variable in the route definition being passed in here
+        pageLoader("always_start_two");
+    },
+    once_restart_two: function( ) {
+        // Note the variable in the route definition being passed in here
+        console.log("in routes, firing Once_start_two");
+        pageLoader("once_start_two");
+    },
+    span_restart_two: function( ) {
+        // Note the variable in the route definition being passed in here
+        pageLoader("span_start_two");
+    },
+    always_restart_two: function( ) {
+        // Note the variable in the route definition being passed in here
+        pageLoader("always_start_two");
     },
     about: function( ) {
         // Note the variable in the route definition being passed in here
         pageLoader("about");
     },
-    defaultRoute: function( ){
+    defaultRoute: function( ) {
         pageLoader("home");
-        
+
     }
 });
            
